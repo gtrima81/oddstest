7e51d5094be0a883fd18022f22cc90e4835a6b91
Giuseppe Trimarchi <giuseppe.trimarchi@connectt.us>
2019-12-05 16:14:46 +0000

    here we go!!!

---
+++ src/main/java/oddtest/services/com/App.java
@@ -0,0 +1,11 @@
+package oddtest.services.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
